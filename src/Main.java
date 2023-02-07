1f2b4b39a33e3645d90b0103ca80f085196f6ef5
e.naydenova <e.naydenova@corp.mail.ru>
2023-02-07 20:24:43 +0300

    Fix main

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,18 @@
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
 
-        String textParam = "fgfghgh123";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите параметры:");
+        String textParam = sc.nextLine();
 
-        Pattern pattern = Pattern.compile("\\d+");
+        Pattern pattern = Pattern.compile("\\d[0-9]+\\d");
         Matcher matcher = pattern.matcher(textParam);
-        int start = 0;
-        while (matcher.find(start)) {
-            String value = textParam.substring(matcher.start(), matcher.end());
-            int resultParam = Integer.parseInt(value);
-            System.out.printf("Результат переведен в число = ", resultParam);
-            String resultInt = String.join("", String.valueOf(resultParam).split(""));
-            int i = Integer.parseInt (resultInt);
-            int a = i /100;
-            int b = i %100/10;
-            int c = i%10;
-            System.out.printf("Сумма найденных чисел", a+b+c);
-            start = matcher.end();
+        while (matcher.find()) {
+            System.out.println("Найдено от " + matcher.start() + " до " + (matcher.end()-1));
         }
     }
 }
