482590739543601a1008cb3ba80b1862095adc1d
e.naydenova <e.naydenova@corp.mail.ru>
2023-03-30 18:52:33 +0300

    add Client

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,55 +1,60 @@
 package client;
 
 import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.net.Socket;
-
-
-import static client.Server.SERVER_URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class Client {
+    public static final int SERVER_PORT = 80;
+    public static final String FILE_TO_SEND = "/Users/katya/Downloads/photo_naydenova.jpg";
     private static DataOutputStream dataOutputStream = null;
     private static DataInputStream dataInputStream = null;
 
-    public static void main(String[] args){
-//        URLConnection connection = new URL(SERVER_URL).openConnection();
-//        connection.setDoOutput(true);
-//        connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-
-        try (Socket socket = new Socket(SERVER_URL, Server.SERVER_PORT)) {
-            dataInputStream = new DataInputStream(socket.getInputStream());
-            dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            System.out.println("Sending the File to the Server");
-
-            sendFile(Server.FILE_TO_SEND);
+    public static String boundary = "===ENaydenova===";
 
 
-            dataInputStream.close();
-            dataInputStream.close();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        Socket sock = new Socket(new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("http://194.99.21.219/upload.php", 80)));;
+        String FileName = "/Users/katya/Downloads/photo_naydenova.jpg";
+        File MyFile = new File(FileName);
+        int FileSize = (int) MyFile.length();
+        OutputStream os =sock.getOutputStream();
+        PrintWriter pr = new PrintWriter(sock.getOutputStream(), true);
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(MyFile));
+        Scanner in = new Scanner(sock.getInputStream());
+
+        pr.println(FileName);
+        pr.println(FileSize);
+        byte[] filebyte = new byte[FileSize];
+        bis.read(filebyte, 0, filebyte.length);
+        os.write(filebyte, 0, filebyte.length);
+        System.out.println(in.nextLine());
+        os.flush();
+        sock.close();
+
+//      try (Socket socket = new Socket("http://194.99.21.219/upload.php")) {
+
+//        try (Socket socket = new Socket(new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("http://194.99.21.219/upload.php", 80)));) {
+//            dataInputStream = new DataInputStream(socket.getInputStream());
+//            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+//            System.out.println("Sending the File to the Server");
+//
+//
+//            dataInputStream.close();
+//            dataInputStream.close();
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
-    // sendFile function define here
-    private static void sendFile(String path) throws Exception {
-        int bytes;
-        // Open the File where he located in your pc
-        File file = new File(path);
-        FileInputStream fileInputStream = new FileInputStream(file);
 
-        // Here we send the File to Server
-        dataOutputStream.writeLong(file.length());
-        // Here we  break file into chunks
-        byte[] buffer = new byte[10000000];
-        while ((bytes = fileInputStream.read(buffer)) != -1) {
-            // Send the file to Server Socket
-            dataOutputStream.write(buffer, 0, bytes);
-            dataOutputStream.flush();
-        }
-        // close the file here
-        fileInputStream.close();
-    }
 }
 
 
